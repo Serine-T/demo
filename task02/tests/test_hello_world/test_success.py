1e3220d5e8f088abf2af5fe5994190513e55cd4b
serine-t <serinetovmasyan98@gmail.com>
2025-02-24 02:59:55 +0400

    task02

--- task02/tests/test_hello_world/test_success.py
+++ task02/tests/test_hello_world/test_success.py
@@ -3,33 +3,27 @@ from tests.test_hello_world import HelloWorldLambdaTestCase
 
 class TestSuccess(HelloWorldLambdaTestCase):
     def test_success(self):
-        # Mocking a valid event for the /hello GET request
         event = {
             'path': '/hello',
             'httpMethod': 'GET'
         }
-        # Call the Lambda handler and assert the successful response
         response = self.HANDLER.handle_request(event, dict())
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(response['body'], '{"message": "Hello from Lambda"}')
 
     def test_bad_request(self):
-        # Mocking an invalid event with a different path
         event = {
-            'path': '/student_id',  # Invalid path
-            'httpMethod': 'GET'  # Valid method, but the path is invalid
+            'path': '/student_id',
+            'httpMethod': 'GET'  
         }
-        # Call the Lambda handler and assert the error response
         response = self.HANDLER.handle_request(event, dict())
         self.assertEqual(response['statusCode'], 400)
         self.assertTrue('Bad request syntax or unsupported method' in response['body'])
         
-        # Mocking an invalid event with a different HTTP method
         event = {
             'path': '/hello',
-            'httpMethod': 'POST'  # Invalid method, should be GET
+            'httpMethod': 'POST' 
         }
-        # Call the Lambda handler and assert the error response
         response = self.HANDLER.handle_request(event, dict())
         self.assertEqual(response['statusCode'], 400)
         self.assertTrue('Bad request syntax or unsupported method' in response['body'])
