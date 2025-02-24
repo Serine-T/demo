3c01cd29870118efa7c876e3b9bf3e8458aad447
Serine-Tovmasyan <serine.tovmasyan@movato.com>
2025-02-24 17:19:59 +0400

    test

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -1,28 +1,35 @@
 import logging
 import json
 
+# def handle_request(event, context):
+#     try:
+#         path = event.get("path")
+#         method = event.get("httpMethod")
+
+#         if path == "/hello" and method == "GET":
+#             return {
+#                 "statusCode": 200,
+#                 "message": "Hello from Lambda"
+#             }
+
+#         return {
+#             "statusCode": 400,
+#             "message": "Bad request syntax or unsupported method. Request path: /cmtr-5f9b79e5. HTTP method: GET"
+#         }
+
+#     except Exception as e:
+#         logging.error(f"Unexpected error: {str(e)}")
+#         return {
+#             "statusCode": 500,
+#             "message": "Internal server error"
+#         }
+
 def handle_request(event, context):
-    try:
-        path = event.get("path")
-        method = event.get("httpMethod")
+    return {
+        "statusCode": 200,
+        "message": "Hello from Lambda"
+    }
 
-        if path == "/hello" and method == "GET":
-            return {
-                "statusCode": 200,
-                "message": "Hello from Lambda"
-            }
-
-        return {
-            "statusCode": 400,
-            "message": "Bad request syntax or unsupported method. Request path: /cmtr-5f9b79e5. HTTP method: GET"
-        }
-
-    except Exception as e:
-        logging.error(f"Unexpected error: {str(e)}")
-        return {
-            "statusCode": 500,
-            "message": "Internal server error"
-        }
 
 def lambda_handler(event, context):
     response = handle_request(event, context)
